5fddf8e6d656e0439f9e74d633d2f216bdf154bc
斑马 <banma@maihaoche.com>
2017-03-14 20:21:13 +0800

    心增test

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by banma on 17/2/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<Object>();
+        list.add(new Integer(1));
+        System.out.println(list.get(0));
+    }
+}
