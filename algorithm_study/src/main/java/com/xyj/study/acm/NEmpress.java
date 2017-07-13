0dd1e03737e33c766b231aed382f074aff345d0f
banma <21409714@qq.com>
2017-07-13 19:41:12 +0800

    N皇后正确解法

--- algorithm_study/src/main/java/com/xyj/study/acm/NEmpress.java
+++ algorithm_study/src/main/java/com/xyj/study/acm/NEmpress.java
@@ -1,5 +1,7 @@
 package com.xyj.study.acm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,20 +10,29 @@ import java.util.Scanner;
  */
 public class NEmpress {
 
-    private static int count;
+    private static long count;
 
     public static void main(String[] args) {
         int N = new Scanner(System.in).nextInt();
-        //先全部设为00
-        int[][] a = new int[N][N];
-        for (int i = 0; i < a.length; i++) {
-            for (int k = 0; k < a[i].length; k++) {
-                a[i][k] = 0;
+        List<Long> list = new ArrayList<>();
+        while (N != 0) {
+            //先全部设为00
+            int[][] a = new int[N][N];
+            for (int i = 0; i < a.length; i++) {
+                for (int k = 0; k < a[i].length; k++) {
+                    a[i][k] = 0;
+                }
             }
+            //开始递归最优解！
+            search(0, a);
+            list.add(count);
+            count = 0;
+            N = new Scanner(System.in).nextInt();
         }
-        //开始递归最优解！
-        search(0, a);
-        System.out.println(N + "皇后的最优解有：" + count + "种");
+        for (Long a : list) {
+            System.out.println(a);
+        }
+//        System.out.println(N + "皇后的最优解有：" + count + "种");
     }
 
     //美滋滋
@@ -29,7 +40,7 @@ public class NEmpress {
 
         //到达了最后说明这条解可行！ 打印并+1
         if (y == a.length) {
-            //输出结果！
+//            //输出结果！
 //            for (int m = 0; m < a.length; m++) {
 //                for (int n = 0; n < a.length; n++) {
 //                    System.out.printf(a[m][n] + "");
@@ -70,20 +81,28 @@ public class NEmpress {
             }
         }
         //判断左上
-         if(x - 1 >= 0 && y - 1 >= 0 && a[x - 1][y - 1] == 1) {
-            return false;
+        for (m = x - 1, n = y - 1; m >= 0 && n >= 0; m--, n--) {
+            if (a[m][n] == 1) {
+                return false;
+            }
         }
         //判断左下
-        if (x <= a.length - 2 && y - 1 >= 0 && a[x + 1][y - 1] == 1) {
-            return false;
+        for (m = x + 1, n = y - 1; m < a.length && n >= 0; m++, n--) {
+            if (a[m][n] == 1) {
+                return false;
+            }
         }
         //判断右上
-        if (x - 1 >= 0 && y <= a.length - 2 && a[x - 1][y + 1] == 1) {
-            return false;
+        for (m = x - 1, n = y + 1; m >= 0 && n < a.length; m--, n++) {
+            if (a[m][n] == 1) {
+                return false;
+            }
         }
         //判断右下
-        if (x <= a.length - 2 && y <= a.length - 2 && a[x + 1][y + 1] == 1) {
-            return false;
+        for (m = x + 1, n = y + 1; m < a.length && n < a.length; m++, n++) {
+            if (a[m][n] == 1) {
+                return false;
+            }
         }
         return true;
     }
