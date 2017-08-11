29c12027c7201f20805b40c706a921652f07e33d
banma <21409714@qq.com>
2017-08-11 16:52:59 +0800

    添加 scala 学习 模板

--- hadoop_study/src/main/java/com/xyj/study/service/FileService.java
+++ hadoop_study/src/main/java/com/xyj/study/service/FileService.java
@@ -4,4 +4,7 @@ package com.xyj.study.service;
  * Created by banma on 2017/8/2.
  */
 public interface FileService {
+
+    int mkdir(String path) throws Exception;
+
 }
--- hadoop_study/src/main/java/com/xyj/study/service/impl/FileServiceImpl.java
+++ hadoop_study/src/main/java/com/xyj/study/service/impl/FileServiceImpl.java
@@ -6,7 +6,10 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by banma on 2017/8/2.
@@ -15,13 +18,29 @@ public class FileServiceImpl implements FileService {
 
     @Test
     public void uploadFile() throws Exception{
+        System.setProperty("hadoop.home.dir", "/home/hadoop");
         Configuration conf = new Configuration();
-        String hdfsPath = "hdfs://localhost:50070/";
-        String hdfsInput = "hdfs://localhost:50070/";
-        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
-        fs.copyFromLocalFile(new Path("~/docker/volumes/word"), new Path(hdfsInput));
+        FileSystem fs = FileSystem.get(URI.create("hdfs://172.21.1.105:9000"), conf, "hadoop");
+        fs.copyFromLocalFile(new Path("/Users/banma/docker/volumes/wordcount_result.txt"), new Path("/bbb/"));
         fs.close();
         System.out.println("已经上传文件到input文件夹啦");
     }
+
+    @Test
+    public void mkdir() throws Exception{
+
+        String path = "/bbb";
+        Configuration conf = new Configuration();
+        URI uri = new URI(path);
+        FileSystem fileSystem = FileSystem.get(uri, conf, "hadoop");
+        Path filePath = new Path(path);
+        fileSystem.mkdirs(filePath);
+    }
+
+    @Override
+    public int mkdir(String path) throws Exception {
+        return 0;
+    }
+
 
 }
